20128107710fa87c7d72e9ed1e3121de0fcc9923
Admiral-401 <ericmureithi401@gmail.com>
2026-01-13 03:59:33 +0300

    CTA under dev process

--- main.js
+++ main.js
@@ -12,9 +12,20 @@ menuToggle.addEventListener("click", () => {
   menuToggle.classList.toggle("active");
 });
 
+document.addEventListener("click", (event) => {
+  if (!menuToggle.contains(event.target) && !navigation.contains(event.target)) {
+    navigation.classList.remove("active");
+    menuToggle.classList.remove("active");
+  }
+});
+
 let index = 0;
 
 setInterval(() => {
+  if(!slides.length) return;
+  if(index > slides.length){
+    slides.length = 0;
+  }
   slides[index].classList.remove("active");
   index = (index + 1) % slides.length;
   slides[index].classList.add("active");
